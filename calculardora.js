c32a3391c7535ce348d0d47286c75eb2f6b89d98
amadeoponce <amfiis@gmail.com>
2019-04-18 10:06:10 -0500

    funcion restar  multip

--- calculardora.js
+++ calculardora.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var suma1 = function (valor1, valor2){
-    return valor1 + valor2;
-};
- 
-module.exports = suma1;
-
 module.exports.suma3 = function (valor1, valor2){
     return valor1 + valor2;
+}
+module.exports.restar = function (valor1, valor2){
+    return valor1 - valor2;
+}
+module.exports.multiplicar = function (valor1, valor2){
+    return valor1 * valor2;
 }
